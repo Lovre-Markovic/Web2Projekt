8e36195df6ead91aa20b6989ad8624778d7cc672
Lovre-Markovic <lovremarkovic01@gmail.com>
2025-10-26 18:26:08 +0100

    Fix session secret error

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     baseURL: process.env.AUTH0_BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
-    secret: process.env.AUTH0_CLIENT_SECRET || process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
   })
 );
 
